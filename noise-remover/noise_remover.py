44bbc929868d815e9d38f8a29e3a4e0d569d2513
Melody CHeng <cchengmelody@gmail.com>
2020-09-27 16:11:21 -0700

    Remove html comments

--- noise-remover/noise_remover.py
+++ noise-remover/noise_remover.py
@@ -5,7 +5,7 @@
 # $ easy_install lxml
 # $ easy_install html5lib (parses HTML the way a web browser does)
 import os
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup as bs, Comment
 
 #=============  Parse a document (can pass string or open filehandle) ==========
 for filename in os.listdir("../repository"):
@@ -13,21 +13,11 @@ for filename in os.listdir("../repository"):
         html_content = f.read()
         f.close()
         soup = bs(html_content, "html.parser")
-        for tags in soup.find_all(["link", "script","style", "a", "input", "nav", "img ", "head", "<!--"]): 
+        # Remove any comments in the html
+        comments = soup.findAll(text=lambda text:isinstance(text, Comment))
+        [comment.extract() for comment in comments]
+        # Remove tags that will not contain content
+        for tags in soup.find_all(["link", "script","style", "a", "input", "nav", "img ", "head", "<!--"]):
             tags.decompose()
         print(soup)
     break
-
-# print(BeautifulSoup("<html><head></head><body>Sacr&eacute; bleu!</body></html>", features="lxml"))
-# <html><head></head><body>Sacré bleu!</body></html>
-
-# ===== Create a BeautifulSoup object with the HTML from GET request to the web page ====
-# vgm_url = 'https://www.vgmusic.com/music/console/nintendo/nes/'
-# html_text = requests.get(vgm_url).text
-# soup = BeautifulSoup(html_text, 'html.parser')
-
-#================== find and find_all =================
-#soup.find() is great for cases where you know there is only one element (ex. body tag)
-#soup.find_all() iterate through all of the hyperlinks on the page and print their URLs:
-# for link in soup.find_all('a'):
-#     print(link.get('href'))
