ec592ac269d70d2a201816f50e3dd19c53ecd81a
Melody CHeng <cchengmelody@gmail.com>
2020-09-27 17:00:41 -0700

    write output to soupText.html, remove div, li, span tags but leave content

--- noise-remover/noise_remover.py
+++ noise-remover/noise_remover.py
@@ -1,23 +1,25 @@
-# ======= For pulling data out of HTML and XML files ======
-# $ easy_install beautifulsoup4 - install beautifulsoup4
-
-# ================== Installing a parser ===================
-# $ easy_install lxml
-# $ easy_install html5lib (parses HTML the way a web browser does)
 import os
 from bs4 import BeautifulSoup as bs, Comment
 
-#=============  Parse a document (can pass string or open filehandle) ==========
-for filename in os.listdir("../repository"):
-    with open(os.path.join("../repository", filename), 'r', encoding="utf8") as f:
+#=============  Parse HTML files from repository folder==========
+for filename in os.listdir('../repository'):
+    with open(os.path.join('../repository', filename), 'r', encoding='utf8') as f:
         html_content = f.read()
         f.close()
-        soup = bs(html_content, "html.parser")
+        soup = bs(html_content, 'html.parser')
         # Remove any comments in the html
         comments = soup.findAll(text=lambda text:isinstance(text, Comment))
         [comment.extract() for comment in comments]
         # Remove tags that will not contain content
-        for tags in soup.find_all(["link", "script","style", "a", "input", "nav", "img ", "head", "<!--"]):
+        for tags in soup.find_all(['link', 'script','style', 'a', 'input', 'nav', 'img ', 'head', '<!--', 'img']):
             tags.decompose()
-        print(soup)
+        # Remove tags, but leave content in the following tags
+        for match in soup.findAll(['li','div', 'span']):
+            match.replaceWithChildren()
+        # Write altered html file to soupText.html in noise-remover folder
+        soup = str(soup)
+        [soup.strip('\n') for s in soup]
+        with open('soupText.html', 'w', encoding='utf-8') as file:
+            file.write(soup)
+            file.close()
     break
