e7ae985a1fbfb7a7f83adda9359801d590304444
Matthew Diep <MatthewDiep514@gmail.com>
2020-09-28 22:26:43 -0700

    reviewed our method for noise removal

--- noise-remover/noise_remover.py
+++ noise-remover/noise_remover.py
@@ -14,18 +14,15 @@ for filename in os.listdir('../repository'):
         doctype = soup.findAll(text=lambda text:isinstance(text, Doctype))
         [doctype.extract() for doctype in doctype]
         # Remove tags that will not contain content
-        for tags in soup.find_all(['link', 'script','style', 'a', 'input', 'nav', 'img ', 'noindex', 'img', 'button', 'video', 'br', 'meta']):
+        for tags in soup.find_all(['link', 'script', 'style', 'input', 'nav', 'noindex', 'img', 'button', 'video', 'br', 'meta']):
             tags.decompose()
-        # Remove tags, but leave content in the following tags
-        # for match in soup.findAll(['li','div', 'span', 'head']):
-        #     match.replaceWithChildren()
-        for match in soup.findAll():
-            match.replaceWithChildren()
+
         # Write altered html files to noise-html-output folder
         soup = str(soup.prettify())
         with open(os.path.join('./noise-html-output', filename), 'w', encoding='utf-8') as file:
             file.write(soup)
             file.close()
+    break
 
 def token_count(tag):
     test_string = tag.text
